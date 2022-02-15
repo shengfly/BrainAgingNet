50c849cf7c478141bfe241594b4295af36e2b061
Sheng He <heshengxgd@gmail.com>
2022-02-15 12:30:58 -0500

    Update Global-Local-Transformer.py

--- Global-Local-Transformer.py
+++ Global-Local-Transformer.py
@@ -171,20 +171,6 @@ class GlobalAttention(nn.Module):
         attention_output = self.proj_dropout(attention_output)
         
         return attention_output
-
-class convBlock(nn.Module):
-    def __init__(self,inplace,outplace,kernel_size=3,padding=1):
-        super().__init__()
-        
-        self.relu = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(inplace,outplace,kernel_size=kernel_size,padding=padding,bias=False)
-        self.bn1 = nn.BatchNorm2d(outplace)
-
-    def forward(self,x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        return x
     
 class Feedforward(nn.Module):
     def __init__(self,inplace,outplace):
